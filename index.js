7727b80edcce141b7f63ae165090a4c479638a7f
kirigizakazutovn <83147867+kirigizakazutovn@users.noreply.github.com>
2021-10-06 20:38:11 +0700

    Tệp main chính
    Cấm sửa bậy bạ

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
+  client.user.setActivity(`Thử nghiệm trên Gitlch bởi yuukidev#4827`, { type: "PLAYING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
